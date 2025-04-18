d931d09e6e4c3e5a5d89ae6eb6e5ba7a330cb9ed
Codyvb <37961637+codyvb@users.noreply.github.com>
2025-04-18 03:59:29 -0600

    common

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -3,12 +3,10 @@ export async function GET() {
 
   const config = {
     accountAssociation: {
-      accountAssociation: {
-        header: "eyJmaWQiOjc0NiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDM3NUMxZEU1Nzk1MTRkOTBEYTcwMjc1MDdFMkM0N0M5MzkzMUQxQjcifQ",
-        payload: "eyJkb21haW4iOiJ3d3cuZ2Vtcy5yaXAifQ",
-        signature: "MHg3YjQ3Y2RmOWMwYWVhMzU0YTFlZmM3MTFjZTlmN2Y2OTI3MTJiZjY5ZDcyZjQ1NjQzYjJjN2UyNGFjOGZkZTI4MDFkMDU2NmQxMTY4MTgxNjI2ZGU0Nzg5ZTg1MGZhMDc3NTIwOGQ3N2QyMWQ3MmY2MzgzOGYxNWRkNjBiODk4ZjFj"
-      },
-    },
+      header: "eyJmaWQiOjc0NiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDM3NUMxZEU1Nzk1MTRkOTBEYTcwMjc1MDdFMkM0N0M5MzkzMUQxQjcifQ",
+      payload: "eyJkb21haW4iOiJ3d3cuZ2Vtcy5yaXAifQ",
+      signature: "MHg3YjQ3Y2RmOWMwYWVhMzU0YTFlZmM3MTFjZTlmN2Y2OTI3MTJiZjY5ZDcyZjQ1NjQzYjJjN2UyNGFjOGZkZTI4MDFkMDU2NmQxMTY4MTgxNjI2ZGU0Nzg5ZTg1MGZhMDc3NTIwOGQ3N2QyMWQ3MmY2MzgzOGYxNWRkNjBiODk4ZjFj"
+    },  
     frame: {
       version: "1",
       name: "Gems",
