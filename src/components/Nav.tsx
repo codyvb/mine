f7d710a763ee475fc531459a487b2554c7e769e9
Codyvb <37961637+codyvb@users.noreply.github.com>
2025-04-22 12:33:24 -0600

    name

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -15,7 +15,7 @@ const Nav: React.FC<NavProps> = ({ active, onOpenLeaderboard, onOpenPrizes }) =>
       onClick={onOpenLeaderboard}
       type="button"
     >
-      Leaderboard
+      Activity
     </button>
     <button
       className={`flex-1 px-4 py-4 bg-neutral-700 hover:bg-neutral-800 text-white rounded-md transition-colors text-sm ${active === 'prizes' ? 'bg-neutral-800' : ''}`}
